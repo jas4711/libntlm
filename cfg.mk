dc6a242f61bf750012285875f5386273c2d11ce3
Simon Josefsson <simon@josefsson.org>
2024-04-11 23:44:48 +0200

    maint: Add srcdist target. Use gnulib mirror.  CA fixes.  Hash tarballs.

--- cfg.mk
+++ cfg.mk
@@ -42,6 +42,9 @@ gl_public_submodule_commit =
 
 # Maintainer targets
 
+srcdist:
+	git archive --prefix=libntlm-1.8/ -o libntlm-1.8-src.tar.gz HEAD
+
 ChangeLog:
 	git2cl > ChangeLog
 	cat .clcopying >> ChangeLog
